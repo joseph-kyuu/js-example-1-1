1cad2383f6f6ad2f1a72a0495d6bd957edbf2543
Joseph_Kyuu <joseph_kyuu@Joseph-KyuudeMacBook-Air.local>
2026-07-21 20:29:30 +0800

    feat:新增 function_example.js 範例

--- javascript/function_example.js
+++ javascript/function_example.js
@@ -154,3 +154,166 @@ function calcBmi(height, weight) {
 let bmiNum2 = +calcBmi(178, 69);
 console.log(bmiNum2);
 console.log(typeof bmiNum2);
+
+/*
+==================
+!!!!! 雨具+if !!!!!
+==================
+*/
+
+/*
+input
+checkWeather("雨天");
+checkWeather("晴天");
+
+output
+要帶雨具
+不用帶雨具
+*/
+
+function checkWeather(weather) {
+  if (weather === "雨天") {
+    console.log("要帶雨具");
+    return "要帶雨具";
+  } else if (weather === "晴天") {
+    console.log("不用帶雨具");
+    return "不用帶雨具";
+  }
+}
+
+let weather1 = checkWeather("雨天");
+let weather2 = checkWeather("晴天");
+
+console.log(weather1);
+console.log(weather2);
+
+/*
+======================
+!!!!! 增加陣列資料 !!!!!
+======================
+*/
+
+/*
+let data = [];
+
+input
+add("hello");
+add("你好嗎？");
+
+output
+["hello","你好嗎？"]
+*/
+
+let addArr = [];
+
+function add(str) {
+  addArr.push(str);
+  console.log(addArr);
+}
+
+add("hello");
+add("你好嗎？");
+
+/*
+=========================
+!!!!! 增加陣列物件資料 !!!!!
+=========================
+*/
+
+/*
+let data = [];
+
+//input
+add("洧杰","男生");
+add("葉子","女生");
+
+// output
+//[
+// {name:"洧杰",gender:"男生"},
+// {name:"葉子",gender:"女生"}
+//]
+*/
+
+let addObjToArr = [];
+
+function addObj(str1, str2) {
+  let obj = {};
+  obj.name = str1;
+  obj.gender = str2;
+  addObjToArr.push(obj);
+  console.log(addObjToArr);
+}
+
+addObj("洧杰", "男生");
+addObj("葉子", "女生");
+
+/*
+=====================
+!!!!! 取物件資料 !!!!!
+=====================
+*/
+
+/*
+const bmiStatesData = {
+  overThin: {
+    state: "過輕",
+    color: "藍色"
+  },
+  normal: {
+    state: "正常",
+    color: "紅色"
+  }
+}
+input
+checkBmiStates("overThin");
+checkBmiStates("normal");
+
+output
+你的體重過輕，指數為藍色
+你的體重正常，指數為紅色
+*/
+
+const bmiStatesData = {
+  overThin: {
+    state: "過輕",
+    color: "藍色",
+  },
+  normal: {
+    state: "正常",
+    color: "紅色",
+  },
+};
+
+function checkBmiStates(status) {
+  if (status === "overThin") {
+    console.log(
+      `你的體重${bmiStatesData.overThin.state}，指數為${bmiStatesData.overThin.color}`,
+    );
+    return `你的體重${bmiStatesData.overThin.state}，指數為${bmiStatesData.overThin.color}`;
+  } else if (status === "normal") {
+    console.log(
+      `你的體重${bmiStatesData.normal.state}，指數為${bmiStatesData.normal.color}`,
+    );
+    return `你的體重${bmiStatesData.normal.state}，指數為${bmiStatesData.normal.color}`;
+  }
+}
+
+let status1 = checkBmiStates("overThin");
+let status2 = checkBmiStates("normal");
+
+console.log(status1);
+console.log(status2);
+
+// 寫法2
+function checkBmiStatus(status) {
+  console.log(
+    `你的體重${bmiStatesData[status].state}，指數為${bmiStatesData[status].color}`,
+  );
+  return `你的體重${bmiStatesData[status].state}，指數為${bmiStatesData[status].color}`;
+}
+
+let status3 = checkBmiStatus("overThin");
+let status4 = checkBmiStatus("normal");
+
+console.log(status3);
+console.log(status4);
