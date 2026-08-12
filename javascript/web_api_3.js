2f8cefd3974311d79d6096770a36d1868733c414
Joseph_Kyuu <joseph_kyuu@Joseph-KyuudeMacBook-Air.local>
2026-08-13 01:28:10 +0800

    feat:新增 web_api_3 的內容

--- javascript/web_api_3.js
+++ javascript/web_api_3.js
@@ -25,9 +25,8 @@ const deleteAllOrders = document.querySelector(".deleteAllOrders");
 ===================
 */
 let ordersListData;
-let ordersProductsData;
+let ordersProductsStr;
 let ordersListStr = "";
-let ordersProductsStr = "";
 
 function getOrdersList() {
   axios
@@ -38,8 +37,62 @@ function getOrdersList() {
     })
     .then(function (res) {
       ordersListData = res.data.orders;
-
       renderOrderList(ordersListData);
+      renderCategoryProportion(ordersListData);
+      renderProductsProportion(ordersListData);
+      // console.log(ordersListData);
+      // console.log(ordersListData[0].products[0].title);
+      // console.log(ordersListData[0].products[0].price);
+      let obj = {};
+      let arr;
+      ordersListData.forEach(function (item) {
+        let data = item.products;
+
+        data.forEach(function (item) {
+          // console.log(item.title);
+          // console.log(item.price);
+          if (obj[item.title] === undefined) {
+            obj[item.title] = item.price;
+          } else {
+            obj[item.title] += item.price;
+          }
+        });
+      });
+
+      arr = Object.entries(obj);
+
+      let sortArr = arr.sort(function (a, b) {
+        return b[1] - a[1];
+      });
+
+      let earnArr = [];
+      let othersTotal = 0;
+      sortArr.forEach(function (item, index) {
+        if (index < 3) {
+          earnArr.push(item);
+        } else {
+          othersTotal += item[1];
+        }
+      });
+      earnArr.push(["其他", othersTotal]);
+      console.log(earnArr);
+
+      var chart = c3.generate({
+        bindto: "#chart3",
+        data: {
+          // iris data from R
+          columns: earnArr,
+          type: "pie",
+        },
+        color: {
+          pattern: ["#DACBFF", "#9D7FEA", "#5434A7", "#301E5F"],
+        },
+        pie: {
+          label: {
+            show: false,
+          },
+        },
+      });
     })
     .catch(function (err) {
       console.log(err);
@@ -48,13 +101,15 @@ function getOrdersList() {
 
 function renderOrderList(data) {
   ordersListStr = "";
-  ordersProductsStr = "";
+  let ordersProductArr = [];
+
   data.forEach(function (orderListData) {
-    ordersProductsData = orderListData.products;
-    ordersProductsData.forEach(function (orderProductsData) {
-      ordersProductsStr += `<p>${orderProductsData.title}</p>`;
-    });
+    // console.log(orderListData.products);
+    // 處理商品內容
+    handleProductItems(orderListData);
+    // 處理日期
     const time = handleDate(orderListData);
+    // 處理訂單內容
     ordersListStr += `
       <tr>
               <td>${orderListData.createdAt}</td>
@@ -77,9 +132,21 @@ function renderOrderList(data) {
   });
 
   orderList.innerHTML = ordersListStr;
-  orderPaidState = document.querySelector(".orderPaidState");
+  // orderPaidState = document.querySelector(".orderPaidState");
 }
 
+// 處理商品內容
+function handleProductItems(data) {
+  ordersProductsStr = "";
+  let ordersProductsData = data.products;
+  // console.log(ordersProductsData);
+
+  ordersProductsData.forEach(function (orderProductsData) {
+    ordersProductsStr += `<p>${orderProductsData.title}*${orderProductsData.quantity}</p>`;
+  });
+}
+
+// 處理日期
 function handleDate(time) {
   const orderCreatedTime = new Date(time.createdAt * 1000);
   const year = orderCreatedTime.getFullYear();
@@ -88,6 +155,94 @@ function handleDate(time) {
   const orderCreatedTimeStr = `${year}/${month}/${day}`;
   return orderCreatedTimeStr;
 }
+
+/*
+=================
+!!!!! 圓餅圖 !!!!!
+=================
+*/
+
+// 類別比重
+function renderCategoryProportion(data) {
+  let categoryProportionObj = {};
+  data.forEach(function (orderProductsData) {
+    // console.log(orderProductsData.products);
+    orderProductsData.products.forEach(function (item) {
+      // console.log(item.category);
+      if (categoryProportionObj[item.category] === undefined) {
+        categoryProportionObj[item.category] = 1;
+      } else {
+        categoryProportionObj[item.category]++;
+      }
+    });
+  });
+
+  categoryProportionArr = Object.entries(categoryProportionObj);
+
+  var chart = c3.generate({
+    bindto: "#chart1",
+    data: {
+      // iris data from R
+      columns: categoryProportionArr,
+      type: "pie",
+    },
+    color: {
+      pattern: ["#DACBFF", "#9D7FEA", "#5434A7"],
+    },
+  });
+}
+
+// 商品比重
+function renderProductsProportion(data) {
+  let productsProportionObj = {};
+  data.forEach(function (orderProductsData) {
+    orderProductsData.products.forEach(function (orderProductData) {
+      // console.log(orderProductData);
+      if (productsProportionObj[orderProductData.title] === undefined) {
+        productsProportionObj[orderProductData.title] = 1;
+      } else {
+        productsProportionObj[orderProductData.title]++;
+      }
+    });
+  });
+  let productsProportionObjToArr = Object.entries(productsProportionObj);
+  let productsProportionArr = [];
+  let others = 0;
+  productsProportionObjToArr.forEach(function (item) {
+    if (
+      item[0] === "Louvre 雙人床架／雙人加大" ||
+      item[0] === "Antony 雙人床架／雙人加大" ||
+      item[0] === "Charles 雙人床架" ||
+      item[0] === "Jordan 雙人床架／雙人加大"
+    ) {
+      productsProportionArr.push(item);
+    } else {
+      others++;
+    }
+  });
+  productsProportionArr.push(["其他", others]);
+
+  // categoryProportionArr = Object.entries(productsProportionObj);
+
+  var chart = c3.generate({
+    bindto: "#chart2",
+    data: {
+      // iris data from R
+      columns: productsProportionArr,
+      type: "pie",
+    },
+    color: {
+      pattern: ["#DACBFF", "#9D7FEA", "#5434A7", "#301E5F"],
+    },
+    pie: {
+      label: {
+        show: false,
+      },
+    },
+  });
+}
+
+// 商品比重
 
 getOrdersList();
 
@@ -155,7 +310,6 @@ orderList.addEventListener("click", function (e) {
   if (!e.target.classList.contains("orderPaidState")) {
     return;
   }
-  console.dir(e.target);
   axios
     .put(
       `${base}/api/livejs/v1/admin/${path}/orders`,
